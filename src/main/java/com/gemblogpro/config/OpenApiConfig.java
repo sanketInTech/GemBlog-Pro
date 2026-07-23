fa354eb0c1749085259c53c6b3857dabee9ad825
sanketInTech <sanketdesai1971@gmail.com>
2026-07-23 22:33:02 +0530

    fix swagger bearer authentication

--- src/main/java/com/gemblogpro/config/OpenApiConfig.java
+++ src/main/java/com/gemblogpro/config/OpenApiConfig.java
@@ -3,17 +3,22 @@ package com.gemblogpro.config;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.context.annotation.Configuration;
 
+
 @Configuration
+
 @OpenAPIDefinition(
         info = @Info(
                 title = "GemBlog Pro API",
                 version = "1.0",
                 description = "REST API for the GemBlog Pro blogging platform."
-        )
+        ),
+        security = @SecurityRequirement(name = "Bearer Authentication")
 )
+
 @SecurityScheme(
         name = "Bearer Authentication",
         type = SecuritySchemeType.HTTP,
